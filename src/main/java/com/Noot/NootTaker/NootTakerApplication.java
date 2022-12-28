89264040b19ada32d2569d243136c5978e51cfe7
Justin Z <justin.zhang0403@gmail.com>
2022-12-28 13:02:11 -0500

    Start of Project

---
+++ src/main/java/com/Noot/NootTaker/NootTakerApplication.java
@@ -0,0 +1,13 @@
+package com.Noot.NootTaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NootTakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NootTakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Noot/NootTaker/NootTakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Noot.NootTaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NootTakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
